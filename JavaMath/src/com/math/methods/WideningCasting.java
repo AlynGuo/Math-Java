bac1339f8b9569f5dad08dac78e7763efe7a52ae
Alyssa Guo <guo.alyssalyn@yahoo.com>
2020-04-24 22:09:52 +0800

    initial commit

---
+++ JavaMath/src/com/math/methods/WideningCasting.java
@@ -0,0 +1,12 @@
+package com.math.methods;
+
+public class WideningCasting {
+    
+    public static void main(String[]args){
+        int myInt = 9;
+        double myDouble = myInt;//Automatic casting int to double
+        
+        System.out.println("Int form "+myInt);
+        System.out.println("Double form "+myDouble);
+    }
+}
